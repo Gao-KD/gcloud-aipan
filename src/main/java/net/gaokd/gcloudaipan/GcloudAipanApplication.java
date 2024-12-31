6106201ab5b75c3b66416916e96198abf5a815bc
Gao-KD <819978029@qq.com>
2024-12-31 11:43:12 +0800

    init

---
+++ src/main/java/net/gaokd/gcloudaipan/GcloudAipanApplication.java
@@ -0,0 +1,13 @@
+package net.gaokd.gcloudaipan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcloudAipanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GcloudAipanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/gaokd/gcloudaipan/GcloudAipanApplicationTests.java
@@ -0,0 +1,13 @@
+package net.gaokd.gcloudaipan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcloudAipanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
