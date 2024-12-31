db8447a05f23f5d7b695154fd2a90d84b0c0ab78
Gao-KD <819978029@qq.com>
2024-12-31 12:49:02 +0800

    通用工具类封装

---
+++ src/main/java/net/gaokd/gcloudaipan/exception/BizException.java
@@ -0,0 +1,31 @@
+package net.gaokd.gcloudaipan.exception;
+
+import lombok.Data;
+import net.gaokd.gcloudaipan.enums.BizCodeEnum;
+
+@Data
+public class BizException extends RuntimeException {
+
+    private int code;
+    private String msg;
+    private String detail;
+
+    public BizException(Integer code, String message) {
+        super(message);
+        this.code = code;
+        this.msg = message;
+    }
+
+    public BizException(BizCodeEnum bizCodeEnum) {
+        super(bizCodeEnum.getMessage());
+        this.code = bizCodeEnum.getCode();
+        this.msg = bizCodeEnum.getMessage();
+    }
+
+    public BizException(BizCodeEnum bizCodeEnum, Exception e) {
+        super(bizCodeEnum.getMessage());
+        this.code = bizCodeEnum.getCode();
+        this.msg = bizCodeEnum.getMessage();
+        this.detail = e.toString();
+    }
+}
